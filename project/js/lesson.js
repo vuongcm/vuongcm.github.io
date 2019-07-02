05221ce254d62a4f1a1ce7668aab993903c93e52
vuongcm <vuongcao117@gmail.com>
2019-07-02 21:45:35 +0700

    2/7

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    let i, a, b, c;
+    let i, a, b, c,z, j=0;
     $('.about').css('--height',`${$('.about img').height()}px`);
     for (i=0; i < $('.about').length; i++) {
         b = '#lesson-' + (i+1);
@@ -43,12 +43,12 @@ $(document).ready(function() {
         jsonScience: ['science-page-1','science-page-2']
     };
     let jsonListName = ['jsonLanguage','jsonLogic','jsonMotor','jsonMusic','jsonSkill','jsonScience'];
-        loadJson = function(x, y) {
+    let loadJson = function(x, y) {
             $.ajax({
                 dataType: 'json',
-                url: `data/demo/${y}.json`,
-                success: function(response) {
-                    // let data = JSON.parse(response);
+                url: `data/demo/${y}.json`})
+                .done(function(response) {
+                    //let response = JSON.parse(data);
                     $(`#lesson-${x} .text`).html('');
                     $(`#lesson-${x} aside`).html('<h5>NHIỀU LƯỢT XEM NHẤT</h5>');
                     for (i=0; i < response.user.length; i++) {
@@ -58,25 +58,114 @@ $(document).ready(function() {
                         $(`#lesson-${x} aside`).append(`<div class="hot-pic"><a href="${response.hot[i].a}"><h6>${response.hot[i].h6}</h6></a><div class="view"><p><span></span> ${response.hot[i].seen}</p><p><span></span> ${response.hot[i].cmt}</p></div></div>`);
                     }
                     $(`#lesson-${x} aside`).append(`<div>...</div>`);
-                },
-                error: function() {
+                })
+                .fail(function() {
                     alert('lỗi load dữ liệu');
-                }
-            });
+                });
         }
-    let clickPageNumber = function(x) {
-        $(`#lesson-${x} .number`).removeClass('active');
-        this.addClass('active');
-    }
+    function clickPageNumber(x) {
+        $('.number').eq(x).closest('ul').children('.number').removeClass('active');
+        $('.number').eq(x).addClass('active');
+    };
     loadJson(1,dataList.jsonLanguage[0]);
     loadJson(2,dataList.jsonLogic[0]);
     loadJson(3,dataList.jsonMotor[0]);
     loadJson(4,dataList.jsonMusic[0]);
     loadJson(5,dataList.jsonSkill[0]);
     loadJson(6,dataList.jsonScience[0]);
-    for (key in $('.number')) {
-        $('.number').eq(key).on('click', function(){
-            
+
+    for (i=0; i < $('.number a').length; i++) {
+        $($('.number a')[i]).on('click', function() {
+            $(this).closest('ul').children('.number').removeClass("active");
+            $(this).parent('.number').addClass("active");
         });
     }
+
+    /*for (j=0; j < $('#lesson-1 .number a').length; j++) {
+        $($('#lesson-1 .number a')[j]).on('click', function() {
+            loadJson(1, dataList.jsonLanguage[j]);
+        });
+    }*/
+    $($('#lesson-1 .number a')[0]).on('click', function() {
+        loadJson(1, dataList[jsonListName[0]][0]);
+    });
+    $($('#lesson-1 .number a')[1]).on('click', function() {
+        loadJson(1, dataList[jsonListName[0]][1]);
+    });
+    $($('#lesson-2 .number a')[0]).on('click', function() {
+        loadJson(2, dataList[jsonListName[1]][0]);
+    });
+    $($('#lesson-2 .number a')[1]).on('click', function() {
+        loadJson(2, dataList[jsonListName[1]][1]);
+    });
+    $($('#lesson-3 .number a')[0]).on('click', function() {
+        loadJson(3, dataList[jsonListName[2]][0]);
+    });
+    $($('#lesson-3 .number a')[1]).on('click', function() {
+        loadJson(3, dataList[jsonListName[2]][1]);
+    });
+    $($('#lesson-4 .number a')[0]).on('click', function() {
+        loadJson(4, dataList[jsonListName[3]][1]);
+    });
+    $($('#lesson-4 .number a')[1]).on('click', function() {
+        loadJson(4, dataList[jsonListName[3]][1]);
+    });
+    $($('#lesson-5 .number a')[0]).on('click', function() {
+        loadJson(5, dataList[jsonListName[4]][0]);
+    });
+    $($('#lesson-5 .number a')[1]).on('click', function() {
+        loadJson(5, dataList[jsonListName[4]][1]);
+    });
+    $($('#lesson-6 .number a')[0]).on('click', function() {
+        loadJson(6, dataList[jsonListName[5]][0]);
+    });
+    $($('#lesson-6 .number a')[1]).on('click', function() {
+        loadJson(6, dataList.jsonScience[1]);
+    });
+    for (j=0; j<6; j++) {
+        $($('.content ul li:first-child a')[j]).on('click', function() {
+            $(this).closest('ul').children('.number').removeClass("active");
+            $(this).parent('.page-item').next().addClass("active");
+        });
+        $($('.content ul li:last-child a')[j]).on('click', function() {
+            $(this).closest('ul').children('.number').removeClass("active");
+            $(this).parent('.page-item').prev().addClass("active");
+        });
+    }
+    $('#lesson-1 ul li:first-child a').on('click', function() {
+        loadJson(1, dataList[jsonListName[0]][0]);
+    });
+    $('#lesson-1 ul li:last-child a').on('click', function() {
+        loadJson(1, dataList[jsonListName[0]][1]);
+    });
+    $('#lesson-2 ul li:first-child a').on('click', function() {
+        loadJson(2, dataList[jsonListName[1]][0]);
+    });
+    $('#lesson-2 ul li:last-child a').on('click', function() {
+        loadJson(2, dataList[jsonListName[1]][1]);
+    });
+    $('#lesson-3 ul li:first-child a').on('click', function() {
+        loadJson(3, dataList[jsonListName[2]][0]);
+    });
+    $('#lesson-3 ul li:last-child a').on('click', function() {
+        loadJson(3, dataList[jsonListName[2]][1]);
+    });
+    $('#lesson-4 ul li:first-child a').on('click', function() {
+        loadJson(4, dataList[jsonListName[3]][1]);
+    });
+    $('#lesson-4 ul li:last-child a').on('click', function() {
+        loadJson(4, dataList[jsonListName[3]][1]);
+    });
+    $('#lesson-5 ul li:first-child a').on('click', function() {
+        loadJson(5, dataList[jsonListName[4]][0]);
+    });
+    $('#lesson-5 ul li:last-child a').on('click', function() {
+        loadJson(5, dataList[jsonListName[4]][1]);
+    });
+    $('#lesson-6 ul li:first-child a').on('click', function() {
+        loadJson(6, dataList[jsonListName[5]][0]);
+    });
+    $('#lesson-6 ul li:last-child a').on('click', function() {
+        loadJson(6, dataList.jsonScience[1]);
+    });
 });
