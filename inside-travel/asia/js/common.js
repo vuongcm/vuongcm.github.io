08640ce1ec7c2198aa618e399e6247ed6b9144f3
vuongcm <vuongcao117@gmail.com>
2019-10-14 20:43:26 +0700

    lll

--- inside-travel/asia/js/common.js
+++ inside-travel/asia/js/common.js
@@ -1,6 +1,6 @@
-/*$(document).ready(function() {
+$(document).ready(function() {
 	$('#menu-lv1-1 a').hover(function(){
-		$(this).parent('#menu-lv1-1').nextAll('.sub-menu-destinations').toggleClass('open-flex');
+		$('#menu-lv1-1').nextAll('.sub-menu-destinations').toggleClass('open-flex');
 	});
 	$('.sub-menu-destinations').hover(function(){
         $(this).css('display','flex');
@@ -9,4 +9,19 @@
         $(this).css('display','none');
         $('#menu-lv1-1 a').removeClass('open-opacity');
 	});
-});*/
+
+	$('#menu-lv1-2 a').hover(function(){
+		$('#menu-lv1-2').nextAll('.sub-menu-travel').toggleClass('open-flex');
+	});
+	$('.sub-menu-travel').hover(function(){
+        $(this).css('display','flex');
+        $('#menu-lv1-2 a').addClass('open-opacity');
+    },function(){
+        $(this).css('display','none');
+        $('#menu-lv1-2 a').removeClass('open-opacity');
+	});
+
+});
+window.onload = function() {
+	$('.map-home').height($('.map-home img').height());
+};
