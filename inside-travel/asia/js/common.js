68e55623e2ddef4df73e68755378276fba5627ff
vuongcm <vuongcao117@gmail.com>
2019-10-31 21:13:30 +0700

    aaaaa

--- inside-travel/asia/js/common.js
+++ inside-travel/asia/js/common.js
@@ -136,6 +136,11 @@ $(document).ready(function() {
     context.beginPath();
 	context.arc(450,382,58,Math.PI/2,Math.PI/1.04);
     context.stroke();*/
+    $('.best-support article').hover(function(){
+    	$(this).children('.paragraph').css('max-height','132px');
+    },function(){
+    	$(this).children('.paragraph').removeAttr('style');
+    });
 	// slick map home
 	if ($('#slick-slider-map').length > 0) {
 		$(`#slick-slider-map .gallery`).slick({
