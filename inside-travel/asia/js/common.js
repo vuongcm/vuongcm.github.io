a741efab074ba9d18dec8fcd395a2cd1871f4e39
vuongcm <vuongcao117@gmail.com>
2020-02-15 11:38:26 +0700

    cvb

--- inside-travel/asia/js/common.js
+++ inside-travel/asia/js/common.js
@@ -145,7 +145,7 @@ $(document).ready(function() {
 	}
 	if(bodyWidth < 1025){
     	$('.best-support article').hover(function(){
-	    	$(this).children('.paragraph').css('max-height','132px');
+	    	$(this).children('.paragraph').css('max-height','200px');
 	    },function(){
 	    	$(this).children('.paragraph').removeAttr('style');
 	    });
