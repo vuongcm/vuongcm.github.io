758a5aba77f110d907d4eb2c31f63316b21431f0
vuongcm <vuongcao117@gmail.com>
2020-03-06 16:03:48 +0700

    dcv

--- inside-travel/asia_vs7/js/common.js
+++ inside-travel/asia_vs7/js/common.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	if($('.lazyload').length>0){jQuery(".lazyload").lazyload({effect:"fadeIn"});}
 	//===========header===================
 	let headerHeight = $('header').height();
-	if($('body').attr('class').search('body-st2') == -1){
+	if(!$('body.body-st2').length){
 		$('header').next('section').css('padding-top',`${headerHeight}px`);
 	}
 	let activeParallax = $('.bg-parallax').length ? $('.bg-parallax').attr('class').search('bg-about') == -1 : false;
